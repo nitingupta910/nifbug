dbf3a83572efd77de5875aceaeb7074da4b5950b
Nitin Gupta <nitingupta910@gmail.com>
2018-05-07 12:31:07 -0700

    move native defs to NifIo module directly

--- apps/nif_io/lib/nif_io.ex
+++ apps/nif_io/lib/nif_io.ex
@@ -1,2 +1,7 @@
 defmodule NifIo do
+  use Rustler, otp_app: :nif_io, crate: :io
+
+  def foo(), do: error()
+
+  defp error, do: :erlang.nif_error(:nif_not_loaded)
 end
--- apps/nif_io/lib/nif_io/native.ex
+++
@@ -1,7 +0,0 @@
-defmodule NifIo.Native do
-  use Rustler, otp_app: :nif_io, crate: :io
-
-  def foo(), do: error()
-
-  defp error, do: :erlang.nif_error(:nif_not_loaded)
-end
